1586d85886108f515d09f529683c687370b45767
shilangyu <xmarcinmarcin@gmail.com>
2019-09-22 14:19:37 +0200

    added templating

---
+++ src/templates.ts
@@ -0,0 +1,41 @@
+import escapeHTML from 'escape-html'
+
+export const embed = (
+	strings: TemplateStringsArray,
+	...data: (string[] | string)[]
+) => {
+	let cumm = ''
+	let safe: boolean
+
+	for (let i = 0; i < strings.length + data.length; i++) {
+		const staticString = !(i % 2)
+		let curr = staticString ? strings[i / 2] : data[(i - 1) / 2]
+
+		if (Array.isArray(curr))
+			cumm += safe ? curr.join(' ') : escapeHTML(curr.join(' '))
+		else cumm += staticString ? curr.replace(/\/$/, '') : escapeHTML(curr)
+
+		safe = staticString ? (curr as string).endsWith('/') : false
+	}
+
+	return cumm
+}
+
+export const table = (headers: string[], rows: string[][]) => embed`
+	<table>
+		<thead>
+    	<tr>
+      	/${headers.map(text => embed`<td>${text}</td>`)}
+    	</tr>
+  	</thead>
+		<tbody>
+			/${rows.map(
+				row => embed`
+				<tr>
+					/${row.map(column => embed`<td>${column}</td>`)}
+				</tr>
+			`
+			)}
+  	</tbody>
+	</table>
+`
