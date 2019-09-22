442fa09f5c1ff56c5a9c2b6a8f4882e70b0266ef
shilangyu <xmarcinmarcin@gmail.com>
2019-09-22 22:02:55 +0200

    added Title enum

--- src/store.ts
+++ src/store.ts
@@ -4,5 +4,13 @@ class Store {
 	@observable searchTerm = ''
 	@observable searchContext: keyof ISubmission = 'riderName'
 }
+export enum Title {
+	timestamp = 'Timestamp',
+	platform = 'Platform',
+	riderName = 'Rider name',
+	riderLevel = 'Rider level',
+	contactInfo = 'Contact info',
+	extraInfo = 'Extra info'
+}
 
 export const store = new Store()
