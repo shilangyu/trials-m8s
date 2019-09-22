d103c1d9ffaf28d2378913a413629c7e2ffc15dd
shilangyu <xmarcinmarcin@gmail.com>
2019-09-22 17:26:19 +0200

    importing minified

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import 'materialize-css'
 import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
 import { h, render } from 'preact'
 import App from './components/App'
 
