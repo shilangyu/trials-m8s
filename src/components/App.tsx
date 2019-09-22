8f25dd2673c23375289d7dba2a3e34c483733fae
shilangyu <xmarcinmarcin@gmail.com>
2019-09-22 16:10:03 +0200

    added search input

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,6 +7,7 @@ interface Props {}
 
 const App: FunctionComponent<Props> = () => {
 	const [csv, setCsv] = useState([[], []])
+	const [searchTerm, setSearchTerm] = useState('')
 
 	useEffect(() => {
 		fetch(
@@ -16,7 +17,19 @@ const App: FunctionComponent<Props> = () => {
 			.then(csv => setCsv(CSVToArray(csv)))
 	}, [])
 
-	return <Table headers={csv[0]} rows={csv.slice(1)} />
+	return (
+		<div>
+			<input
+				type="search"
+				placeholder="search..."
+				onKeyUp={({ target }) =>
+					setSearchTerm((target as HTMLInputElement).value)
+				}
+				value={searchTerm}
+			/>
+			<Table headers={csv[0]} rows={csv.slice(1)} filter={searchTerm} />
+		</div>
+	)
 }
 
 export default App
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,34 +1,32 @@
 import { FunctionComponent, h } from 'preact'
-import { useState } from 'preact/hooks'
 
 interface Props {
+	filter: string
 	headers: string[]
 	rows: string[][]
 }
 
-const Table: FunctionComponent<Props> = ({ headers, rows }) => {
-	const [filter, setFilter] = useState('')
-
-	return (
-		<table>
-			<thead>
-				<tr>
-					{headers.map(text => (
-						<td>{text}</td>
-					))}
-				</tr>
-			</thead>
-			<tbody>
-				{rows.map(row => (
+const Table: FunctionComponent<Props> = ({ headers, rows, filter }) => (
+	<table>
+		<thead>
+			<tr>
+				{headers.map(text => (
+					<td>{text}</td>
+				))}
+			</tr>
+		</thead>
+		<tbody>
+			{rows
+				.filter(row => row[2].includes(filter))
+				.map(row => (
 					<tr>
 						{row.map(column => (
 							<td>{column}</td>
 						))}
 					</tr>
 				))}
-			</tbody>
-		</table>
-	)
-}
+		</tbody>
+	</table>
+)
 
 export default Table
