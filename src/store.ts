f27019efd1e26767b599a22280fc2c0bb63118ac
shilangyu <xmarcinmarcin@gmail.com>
2020-08-07 14:32:08 +0000

    workaround for google sheet randomly not enabling CORS

--- src/store.ts
+++ src/store.ts
@@ -22,7 +22,7 @@ class Store {
 
 	fetchSubmissions = async () => {
 		const res = await fetch(
-			'https://docs.google.com/spreadsheets/d/e/2PACX-1vTaxfYW_hASjz5OPYRXyjaa9PMpw9yKmGsUp_QK6clbh1GUaVvsu3gVefoRhx6no22RbGA5X1wK6JzO/pub?gid=734159276&single=true&output=csv'
+			'https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheets/d/e/2PACX-1vTaxfYW_hASjz5OPYRXyjaa9PMpw9yKmGsUp_QK6clbh1GUaVvsu3gVefoRhx6no22RbGA5X1wK6JzO/pub?gid=734159276&single=true&output=csv'
 		)
 		const csv = await res.text()
 
